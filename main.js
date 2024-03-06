25b653974709bff87f487262f7cd5ecee84e491d
ahmed <ahmed.mohammed.developer@gmail.com>
2024-03-06 09:09:26 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let arr = ["A", "S", "B"];
+
+console.log(arr.length);
+
+console.log(Math.random() * arr.length);
+
+console.log(Math.floor(Math.random() * arr.length));
+console.log(Math.trunc(Math.random() * arr.length));
+
+console.log(arr[Math.floor(Math.random() * arr.length)]);
